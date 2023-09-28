2b02f749b84731e30ef97987338be63a72cd3e87
Andrii Kuzmichov <a.v.kuzmi4@gmail.com>
2023-09-28 22:04:31 +0300

    Updates

---
+++ index.63dfa6d6.js
@@ -0,0 +1,2 @@
+(()=>{let e={hamburger:document.querySelector("[hamburger]")};e.hamburger.addEventListener("click",function(){e.hamburger.classList.toggle("burger-active")})})();
+//# sourceMappingURL=index.63dfa6d6.js.map
