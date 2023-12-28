5dd302eefaf63a2a638b882723d0c1188745a593
Andrii Kuzmichov <a.v.kuzmi4@gmail.com>
2023-12-28 16:06:51 +0200

    fix section slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,29 +1,29 @@
-import { refs } from "./refs";
+import { refs } from './refs';
 const { btnPrev, nextBtn, carouselSlides, carouselRow } = refs;
 
-
 (() => {
-  let index = 1;
+  let index = 0;
 
   width = carouselSlides[0].clientWidth;
-  carouselRow.style.transform = 'translateX(' + (- width * index) + 'px)';
-  
+  carouselRow.style.transform = 'translateX(' + -width * index + 'px)';
+
   nextBtn.addEventListener('click', nextSlide);
   function nextSlide() {
-    if (index >= carouselSlides.length - 1) {
-      return
+    if (index >= carouselSlides.length - 4) {
+      return;
     }
     carouselRow.style.transition = 'transform 0.4s ease-out';
     index++;
-    carouselRow.style.transform = 'translateX(' + (- width * index) + 'px)';
-
+    carouselRow.style.transform = 'translateX(' + -width * index + 'px)';
   }
-  // hamburger.addEventListener('click', toggleModalMobile);
 
-  // function toggleModalMobile() {
-  //   hamburger.classList.toggle('burger-active');
-  //   body.classList.toggle('no-scroll');
-  //   modalNav.classList.toggle('backdrop');
-  //   modalNav.classList.toggle('visually-hidden');
-  // }
+  btnPrev.addEventListener('click', prevSlide);
+  function prevSlide() {
+    if (index < 0) {
+      return;
+    }
+    carouselRow.style.transition = 'transform 0.4s ease-out';
+    index--;
+    carouselRow.style.transform = 'translateX(' + -width * index + 'px)';
+  }
 })();
